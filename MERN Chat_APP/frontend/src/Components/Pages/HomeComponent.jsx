bb810e0018891e81ea7f197e7f5aebc5f2faf3b9
Hirak-Debadhikary <103572541+Hirak-Debadhikary@users.noreply.github.com>
2023-04-13 21:29:59 +0530

    Edit Some Styling to the Login and Signup UI

--- MERN Chat_APP/frontend/src/Components/Pages/HomeComponent.jsx
+++ MERN Chat_APP/frontend/src/Components/Pages/HomeComponent.jsx
@@ -14,23 +14,36 @@ import LoginComponent from "../Authentication/LoginComponent";
 
 const HomeComponent = () => {
   return (
-    <Container maxW="xl" centerContent>
+    <Container maxW="xl" centerContent bg="transparent">
       <Box
         display="flex"
         justifyContent="center"
         p={3}
-        bg="transparent"
+        // bg="transparent"
         w="100%"
-        margin="40px 0px 15px 0px"
+        // margin="40px 0px 0px 0px"
+        marginTop="3rem"
         borderRadius="lg"
         borderWidth="1px"
+        marginBottom="2px"
       >
-        <Text fontSize="4xl" fontFamily="inherit">
-          Chat App
+        {/* <Text fontSize="4xl" fontFamily="inherit">
+          Wellcome to TalkLoop
+        </Text> */}
+        <Text
+          fontSize="4xl"
+          fontFamily="inherit"
+          textAlign="center"
+          fontWeight="bold"
+          color="pink.500"
+          textShadow="1px 1px #FFFFFF"
+          letterSpacing="wide"
+        >
+          Welcome to <span style={{ color: "#2D4CCA" }}>TalkLoop</span>
         </Text>
       </Box>
       <Box
-        bg="transparent "
+        // bg="transparent"
         w="100%"
         p={4}
         borderRadius="lg"
@@ -39,9 +52,31 @@ const HomeComponent = () => {
       >
         {" "}
         <Tabs variant="soft-rounded" colorScheme="green">
-          <TabList mb="1rem">
-            <Tab w="50%">Login</Tab>
-            <Tab w="50%">Register</Tab>
+          <TabList mb="1rem" gap="0.5rem">
+            <Tab
+              size="lg"
+              w="50%"
+              bgGradient="linear(to-r, green.500, blue.400  )"
+              _hover={{
+                bgGradient: "linear(to-l, red.300, green.500 )",
+                border: "1px solid black",
+                color: "black",
+              }}
+            >
+              Login
+            </Tab>
+            <Tab
+              w="50%"
+              size="lg"
+              bgGradient="linear(to-r,blue.400, green.500 )"
+              _hover={{
+                bgGradient: "linear(to-l, red.300,green.500 )",
+                border: "1px solid black",
+                color: "black",
+              }}
+            >
+              Register
+            </Tab>
           </TabList>
           <TabPanels>
             <TabPanel>
@@ -60,3 +95,16 @@ const HomeComponent = () => {
 };
 
 export default HomeComponent;
+// size="lg"
+// colorScheme="blue"
+// width="100%"
+// bgGradient="linear(to-r,pink.200, green.500 )"
+// _hover={{
+//   bgGradient: "linear(to-l, red.300, yellow.100,green.500 )",
+//   border: "1px solid black",
+//   color: "black",
+// }}
+// onClick={handleSubmit}
+// >
+// Login
+// </Button>
